26ec655698d5d98aab3b45ddb1fe4417c17ea1d2
Lester Ong <lesterong776@gmail.com>
2022-11-29 21:06:22 +0800

    Add navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,14 @@
 import Logo from '../assets/Logo';
+import './Navbar.css';
 
 const Navbar = () => (
   <nav>
     <Logo />
+    <div className="nav-links">
+      <span> Work </span>
+      <span> Resume </span>
+      <span> Contact </span>
+    </div>
   </nav>
 );
 
--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,7 +1,7 @@
 import { SectionProps } from '../types/Section';
 
 const Section = ({ color, children }: SectionProps) => (
-  <div className={color}>
+  <div className={`h-screen ${color}`}>
     <div className="mx-auto max-w-6xl px-6">{children}</div>
   </div>
 );
