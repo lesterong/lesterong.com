3b26f865d9fd1ab6155622241349e595494f24b4
Lester Ong <lesterong776@gmail.com>
2022-11-29 20:53:10 +0800

    Update eslint for typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,10 @@ module.exports = {
     'comma-dangle': 0,
     'react/react-in-jsx-scope': 'off',
     'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
     'react/function-component-definition': [
       2,
       {
