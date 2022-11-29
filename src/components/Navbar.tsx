9042ee59ba7dac3d672c00965a4f871bd149534a
Lester Ong <lesterong776@gmail.com>
2022-11-30 01:52:46 +0800

    Fix navbar alignment

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,12 +3,12 @@ import Logo from '../assets/Logo';
 const Navbar = () => {
   const navLink = 'cursor-pointer font-display font-semibold';
   return (
-    <nav className="absolute top-0 left-0 right-0 mx-auto flex w-screen max-w-6xl justify-between py-4 px-6">
+    <nav className="absolute top-0 left-0 right-0 mx-auto flex w-screen max-w-6xl items-center justify-between py-4 px-6">
       <Logo />
       <div className="flex space-x-4">
-        <span className={navLink}> Work </span>
-        <span className={navLink}> Resume </span>
-        <span className={navLink}> Contact </span>
+        <div className={navLink}> Work </div>
+        <div className={navLink}> Resume </div>
+        <div className={navLink}> Contact </div>
       </div>
     </nav>
   );
