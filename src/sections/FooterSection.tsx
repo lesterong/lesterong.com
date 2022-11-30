36424aae70eb21f8462574a65c0e7ab8e56150d5
Lester Ong <lesterong776@gmail.com>
2022-12-01 00:05:24 +0800

    Update footer section

--- src/sections/FooterSection.tsx
+++ src/sections/FooterSection.tsx
@@ -1,13 +1,24 @@
 import Bgcolors from '../types/Bgcolors';
 import Section from './Section';
+import Contact from '../assets/Contact';
 
 const FooterSection = () => (
   <Section color={Bgcolors.Beige}>
     <hr className="border-gray-200" />
-    <div className="max-w-xl">
-      <h2 className="mt-6">Like what you see?</h2>
-      <p className="my-6">I&apos;d love to work with you! Let&apos;s talk.</p>
+    <h2 className="mt-6">Like what you see?</h2>
+    <p className="mt-6">I&apos;d love to work with you! Let&apos;s talk.</p>
+    <div className="mb-6 mt-2 flex space-x-4">
+      <a href="https://www.github.com/lesterong/" target="_blank" rel="noopener noreferrer">
+        <Contact.Github />
+      </a>
+      <a href="https://www.linkedin.com/in/lesterongpc/" target="_blank" rel="noopener noreferrer">
+        <Contact.Linkedin />
+      </a>
+      <a href="mailto:lesterong776@gmail.com" target="_blank" rel="noopener noreferrer">
+        <Contact.Email />
+      </a>
     </div>
+    <p className="mb-6 text-center text-sm text-[#a9a9a9]">&copy; Lester Ong, 2022.</p>
   </Section>
 );
 
