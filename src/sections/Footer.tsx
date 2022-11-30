369e2e7b4f517550f34b49a3c795e133416a9c4a
Lester Ong <lesterong776@gmail.com>
2022-12-01 02:31:23 +0800

    Reduce margin in footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => (
   <Section color={Bgcolors.Beige}>
     <hr className="border-gray-200" />
     <h2 className="mt-6">Like what you see?</h2>
-    <p className="mt-6">I&apos;d love to work with you! Let&apos;s talk.</p>
+    <p className="mt-2">I&apos;d love to work with you! Let&apos;s talk.</p>
     <div className="mb-6 mt-2 flex space-x-4">
       <a href="https://www.github.com/lesterong/" target="_blank" rel="noopener noreferrer">
         <Contact.Github />
