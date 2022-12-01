d0f121e28f6bbfe4504324e609802542c2013bd8
Lester Ong <lesterong776@gmail.com>
2022-12-01 16:52:44 +0800

    Refactor contact link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,20 +3,20 @@ import Logo from '../assets/Logo';
 
 const Navbar = () => {
   const navLink = 'font-display font-semibold transition duration-300 ease-in-out hover:text-indigo-200';
-  const path = useLocation().pathname;
+  const { pathname } = useLocation();
   return (
     <nav className="absolute top-0 left-0 right-0 mx-auto flex w-screen max-w-6xl items-center justify-between py-4 px-6">
       <Link to="/">
         <Logo />
       </Link>
       <div className="flex space-x-4">
-        <Link to="projects" className={`${navLink} ${path === '/projects' ? 'text-indigo-200' : ''}`}>
+        <Link to="projects" className={`${navLink} ${pathname === '/projects' ? 'text-indigo-200' : ''}`}>
           Projects
         </Link>
-        <Link to="resume" className={`${navLink} ${path === '/resume' ? 'text-indigo-200' : ''}`}>
+        <Link to="resume" className={`${navLink} ${pathname === '/resume' ? 'text-indigo-200' : ''}`}>
           Resume
         </Link>
-        <Link to="contact" className={`${navLink} ${path === '/contact' ? 'text-indigo-200' : ''}`}>
+        <Link to="contact" className={`${navLink} ${pathname === '/contact' ? 'text-indigo-200' : ''}`}>
           Contact
         </Link>
       </div>
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,38 +1,49 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import Bgcolors from '../types/Bgcolors';
 import Section from './Section';
 import Contact from '../assets/Contact';
 
-const Footer = () => (
-  <Section color={Bgcolors.Beige}>
-    <hr className="border-gray-200" />
-    <h2 className="mt-6">Like what you see?</h2>
-    <div className="mt-2 flex flex-col justify-between sm:flex-row">
-      <div>
-        <p>I&apos;d love to work with you.</p>
-        <Link to="contact" className="nav-link w-fit">
-          <p className="mb-6 inline-block">
-            Come say hello! <span className="right-arrow inline-block">&#x2192;</span>
-          </p>
-        </Link>
-      </div>
-      <div className="text-left sm:text-right">
-        <p>Let&apos;s connect!</p>
-        <div className="mb-6 mt-2 flex justify-start space-x-4 sm:justify-end">
-          <a href="https://www.github.com/lesterong/" target="_blank" rel="noopener noreferrer">
-            <Contact.Github />
-          </a>
-          <a href="https://www.linkedin.com/in/lesterongpc/" target="_blank" rel="noopener noreferrer">
-            <Contact.Linkedin />
-          </a>
-          <a href="mailto:lesterong776@gmail.com" target="_blank" rel="noopener noreferrer">
-            <Contact.Email />
-          </a>
+const Footer = () => {
+  const { pathname } = useLocation();
+  return (
+    <Section color={Bgcolors.Beige}>
+      <hr className="border-gray-200" />
+      <h2 className="mt-6">Like what you see?</h2>
+      <div className="mt-2 flex flex-col justify-between sm:flex-row">
+        <div>
+          <p>I&apos;d love to work with you.</p>
+          {pathname === '/contact' ? (
+            <a href="#top" className="nav-link w-fit">
+              <p className="mb-6 inline-block">
+                Come say hello! <span className="right-arrow inline-block">&#x2192;</span>
+              </p>
+            </a>
+          ) : (
+            <Link to="contact" className="nav-link w-fit">
+              <p className="mb-6 inline-block">
+                Come say hello! <span className="right-arrow inline-block">&#x2192;</span>
+              </p>
+            </Link>
+          )}
+        </div>
+        <div className="text-left sm:text-right">
+          <p>Let&apos;s connect!</p>
+          <div className="mb-6 mt-2 flex justify-start space-x-4 sm:justify-end">
+            <a href="https://www.github.com/lesterong/" target="_blank" rel="noopener noreferrer">
+              <Contact.Github />
+            </a>
+            <a href="https://www.linkedin.com/in/lesterongpc/" target="_blank" rel="noopener noreferrer">
+              <Contact.Linkedin />
+            </a>
+            <a href="mailto:lesterong776@gmail.com" target="_blank" rel="noopener noreferrer">
+              <Contact.Email />
+            </a>
+          </div>
         </div>
       </div>
-    </div>
-    <p className="mb-6 text-center text-sm text-[#a9a9a9]">&copy; Lester Ong, 2022.</p>
-  </Section>
-);
+      <p className="mb-6 text-center text-sm text-[#a9a9a9]">&copy; Lester Ong, 2022.</p>
+    </Section>
+  );
+};
 
 export default Footer;
