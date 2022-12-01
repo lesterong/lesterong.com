abe537b4d2fdc046ed7b81db8b337f02e5171c91
Lester Ong <lesterong776@gmail.com>
2022-12-01 20:28:14 +0800

    Use react to handle forms

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,8 +1,19 @@
+import React, { useState } from 'react';
 import Section from '../sections/Section';
 import Bgcolors from '../types/Bgcolors';
 
 const Contact = () => {
   document.title = 'Lester Ong | Contact';
+  const [values, setValues] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
   return (
     <Section color={Bgcolors.Beige}>
       <h1 className="mt-16 text-center">Contact</h1>
@@ -17,16 +28,23 @@ const Contact = () => {
         </a>
         .
       </p>
-      <form className="mt-2 mb-8 flex max-w-xl flex-col space-y-2" action="/" method="post">
+      <form className="mt-2 mb-8 flex max-w-xl flex-col space-y-2" method="post">
         <input type="hidden" name="form-name" value="contact" />
         <label htmlFor="name">
-          Name <input type="text" name="name" />
+          Name <input type="text" name="name" required onChange={handleChange} value={values.name} />
         </label>
         <label htmlFor="email">
-          Email <input type="email" name="email" />
+          Email <input type="email" name="email" required onChange={handleChange} value={values.email} />
         </label>
         <label htmlFor="message">
-          Message <textarea className="h-36 resize-none" name="message" />
+          Message
+          <textarea
+            className="h-36 min-h-[144px] resize-y"
+            name="message"
+            onChange={handleChange}
+            value={values.message}
+            required
+          />
         </label>
         <p>
           <button className="nav-link w-max" type="submit">
