007a046871f6756cb5584b6d2e193a229a18e96f
Lester Ong <lesterong776@gmail.com>
2022-12-01 15:00:24 +0800

    Add reduce motion for arrow

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,28 @@ module.exports = {
       transparent: 'transparent',
     },
     extend: {
+      animation: {
+        'bounce-right': 'bounce-right 1.2s infinite',
+        'gradient-shift': 'gradient-shift 3s ease infinite',
+      },
+      keyframes: {
+        'bounce-right': {
+          '0%, 100%': {
+            transform: 'translateX(0)',
+          },
+          '50%': {
+            transform: 'translateX(25%)',
+          },
+        },
+        'gradient-shift': {
+          '0%, 100%': {
+            'background-position': '0% 50%',
+          },
+          '50%': {
+            'background-position': '100% 50%',
+          },
+        },
+      },
       gridTemplateColumns: {
         'auto-fill': 'repeat(auto-fill, 3rem)',
       },
