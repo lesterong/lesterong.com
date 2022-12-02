964972f3ba634802e6f4fddfc0bf5bcb5c2896d2
Lester Ong <lesterong776@gmail.com>
2022-12-02 18:17:14 +0800

    Add figma links for dine inn

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -18,7 +18,12 @@ const projects = [
       'Dine Inn is a e-commerce platform that connects home chefs to their customers. I redesigned their user interface for improved user experience and conversion rates.',
     isFeatured: true,
     imageSource: 'dineinn',
-    links: {},
+    links: {
+      'Desktop Demo':
+        'https://www.figma.com/proto/5kDx51vJ4Z46283g8bP5iC/Dine-Inn-Demo?page-id=661%253A5518&node-id=661%3A8058&viewport=947%252C319%252C0.2&scaling=scale-down&starting-point-node-id=661%3A8058&disable-default-key=&disable-default-keyboard-nav=1&hide-ui=1',
+      'Mobile Demo':
+        'https://www.figma.com/proto/5kDx51vJ4Z46283g8bP5iC/Dine-Inn-Demo?page-id=615%3A100&node-id=755%3A5105&viewport=451%2C622%2C0.59&scaling=scale-down&starting-point-node-id=755%3A5105&disable-default-keyboard-nav=1&hide-ui=1',
+    },
   },
 ];
 
