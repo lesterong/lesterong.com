f2b73d9b617d32928fcbc1886ae081bf28c34744
Lester Ong <lesterong776@gmail.com>
2022-12-03 12:58:50 +0800

    Add button for small screen size

---
+++ src/assets/MenuButton.tsx
@@ -0,0 +1,15 @@
+const MenuButton = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6 fill-none stroke-gray-300 stroke-2"
+    viewBox="0 0 24 24"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <line x1="3" y1="12" x2="21" y2="12" />
+    <line x1="3" y1="6" x2="21" y2="6" />
+    <line x1="3" y1="18" x2="21" y2="18" />
+  </svg>
+);
+
+export default MenuButton;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,91 @@
+import { Menu, Transition } from '@headlessui/react';
+import { Fragment, useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import Logo from '../assets/Logo';
+import MenuButton from '../assets/MenuButton';
 
 const Navbar = () => {
-  const navLink = 'font-display font-semibold transition duration-300 ease-in-out hover:text-indigo-200';
+  const MENU_WIDTH: number = 368;
+  const [isButton, setIsButton] = useState(window.innerWidth < MENU_WIDTH);
+
+  const pages: Array<'projects' | 'resume' | 'contact'> = ['projects', 'resume', 'contact'];
   const { pathname } = useLocation();
+
+  useEffect(
+    () => window.addEventListener('resize', () => setIsButton(window.innerWidth < MENU_WIDTH)),
+    [window.innerWidth]
+  );
+
+  const navButton = (
+    <div className="flex justify-end">
+      <Menu>
+        <Menu.Button>
+          <MenuButton />
+        </Menu.Button>
+        <Transition
+          as={Fragment}
+          enterFrom="opacity-0 scale-75 origin-top-right"
+          enterTo="opacity-100 scale-100 origin-top-right"
+          enter="transition ease-in-out duration-150"
+          leave="transition ease-in-out duration-150"
+          leaveFrom="opacity-100 scale-100 origin-top-right"
+          leaveTo="opacity-0 scale-75 origin-top-right"
+        >
+          <Menu.Items className="absolute z-10 mt-1 flex w-auto cursor-pointer flex-col overflow-auto rounded-md border border-gray-100/30 bg-white py-2 text-base shadow-sm">
+            <Menu.Item>
+              {({ active }) => (
+                <Link
+                  to="/"
+                  className={`py-1 pl-2 pr-12 font-display ${pathname === '/' ? 'font-semibold' : ''} ${
+                    active ? 'bg-indigo-100/30' : 'bg-white'
+                  }`}
+                >
+                  Home
+                </Link>
+              )}
+            </Menu.Item>
+            {pages.map((page) => (
+              <Menu.Item key={page}>
+                {({ active }) => (
+                  <Link
+                    to={page}
+                    className={`py-1 pl-2 pr-12 font-display ${pathname === `/${page}` ? 'font-semibold' : ''} ${
+                      active ? 'bg-indigo-100/30' : 'bg-white'
+                    }`}
+                  >
+                    {page.substring(0, 1).toUpperCase() + page.substring(1).toLowerCase()}
+                  </Link>
+                )}
+              </Menu.Item>
+            ))}
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </div>
+  );
+
+  const navLinks = (
+    <div className="flex space-x-4">
+      {pages.map((page) => (
+        <Link
+          key={page}
+          to={page}
+          className={`font-display font-semibold transition duration-300 ease-in-out hover:text-indigo-200 ${
+            pathname === `/${page}` ? 'text-indigo-200' : ''
+          }`}
+        >
+          {page.substring(0, 1).toUpperCase() + page.substring(1).toLowerCase()}
+        </Link>
+      ))}
+    </div>
+  );
+
   return (
     <nav className="absolute top-0 left-0 right-0 mx-auto flex w-screen max-w-6xl items-center justify-between py-4 px-6">
       <Link to="/">
         <Logo />
       </Link>
-      <div className="flex space-x-4">
-        <Link to="projects" className={`${navLink} ${pathname === '/projects' ? 'text-indigo-200' : ''}`}>
-          Projects
-        </Link>
-        <Link to="resume" className={`${navLink} ${pathname === '/resume' ? 'text-indigo-200' : ''}`}>
-          Resume
-        </Link>
-        <Link to="contact" className={`${navLink} ${pathname === '/contact' ? 'text-indigo-200' : ''}`}>
-          Contact
-        </Link>
-      </div>
+      {isButton ? navButton : navLinks}
     </nav>
   );
 };
