69dcfba3ae78455c4e0b698f0884f2f90631da45
Lester Ong <lesterong776@gmail.com>
2022-12-04 15:27:29 +0800

    Improve footer color contrast

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -44,7 +44,7 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <p className="text-color--secondary mb-6 text-center text-sm">&copy; Lester Ong, 2022.</p>
+      <p className="text-color--footer mb-6 text-center text-sm">&copy; Lester Ong, 2022.</p>
     </Section>
   );
 };
