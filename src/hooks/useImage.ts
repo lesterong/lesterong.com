7d3ca0ccf49d9baa16df7fd26709f81f1ff5919e
Lester Ong <lesterong776@gmail.com>
2023-04-24 13:26:48 +0800

    Use CDN to fetch images

--- src/hooks/useImage.ts
+++ src/hooks/useImage.ts
@@ -2,16 +2,29 @@ import { useEffect, useState } from 'react';
 
 // https://stackoverflow.com/questions/53775936/import-image-dynamically-in-react-component
 const useImage = (fileName: string) => {
+  const baseUrl = 'https://cdn.jsdelivr.net/gh/lesterong/lesterong.com/src/assets/images/';
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [image, setImage] = useState({ png: null, webp: null });
+  const [image, setImage] = useState({ png: '', webp: '' });
 
   useEffect(() => {
     const fetchImage = async () => {
       try {
-        const responsePng = await import(`../assets/images/${fileName}.png`);
-        const responseWebp = await import(`../assets/images/${fileName}.webp`);
-        setImage({ png: responsePng.default, webp: responseWebp.default });
+        const responsePng = await fetch(`${baseUrl}${fileName}.webp`).then((res) => {
+          if (res.ok) {
+            return res.blob();
+          }
+          throw new Error('File not found');
+        });
+        const responseWebp = await fetch(`${baseUrl}${fileName}.png`).then((res) => {
+          if (res.ok) {
+            return res.blob();
+          }
+          throw new Error('File not found');
+        });
+        const pngBlob = URL.createObjectURL(responsePng);
+        const webpBlob = URL.createObjectURL(responseWebp);
+        setImage({ png: pngBlob, webp: webpBlob });
       } catch (err: any) {
         setError(err);
       } finally {
