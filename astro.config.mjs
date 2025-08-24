e66500dd75695e3e209532f2fb3299a57b884be5
Lester Ong <lesterong776@gmail.com>
2025-08-24 17:54:54 +0800

    Revamp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig, fontProviders } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   experimental: {
@@ -11,6 +13,22 @@ export default defineConfig({
         cssVariable: "--font-figtree",
         weights: ["100 900"],
       },
+      {
+        provider: fontProviders.fontsource(),
+        name: "JetBrains Mono",
+        cssVariable: "--font-jetbrains-mono",
+        weights: ["100 900"],
+      },
+      {
+        provider: fontProviders.fontsource(),
+        name: "Playfair Display",
+        cssVariable: "--font-playfair-display",
+        weights: ["100 900"],
+      },
     ],
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
